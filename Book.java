9e050ec805a6a4ce4898cc4c651b175a0ffd6866
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-02-05 23:59:27 -0700

    Add files via upload

--- Book.java
+++ Book.java
@@ -3,6 +3,7 @@
 
 
 import java.lang.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -61,7 +62,7 @@ public class Book
 
 // a book collection class
 
-class BookCollection extends Book
+class BookCollection //extends Book
 {
     //created a vector which stores the book object
     private Vector <Book>library = new <Book>Vector();
@@ -78,6 +79,7 @@ class BookCollection extends Book
     //creates a book object and adds it to the array
     public void addBook(String title, String author)
     {
+
         Book b = new Book();
 
 
@@ -87,7 +89,16 @@ class BookCollection extends Book
 
         library.add(index, b);
         library.elementAt(index).printBook();
+
+
+
+
+        // i want to put this method here, but im still working on a bubble sort
+        //this.organiseBooks();
         index++;
+
+
+
     }
     //prints the list of objects in the vector using the book class
     public void viewLibrary()
@@ -107,66 +118,98 @@ class BookCollection extends Book
 
     private int findBook(String title)
     {
-        int location = 0;
-        for(int i = 0; i < index; i++)
-        {
+        //if negative 1 is returned, title not found
+        int location = -1;
 
-            System.out.println (title);
-           if (title.equals( library.elementAt(i).getTitle()))
-            {
-                System.out.println(title + "is at position " + i);
-                 location = i;
-            }else
-           {
-               System.out.println("Book not in library. Check spelling.");
-           }
+            for (int i = 0; i < index; i++) {
 
-        }
+                //System.out.println (title);
+                if (title.equals(library.elementAt(i).getTitle())) {
+                    System.out.println(title + "is at position " + i);
+                    location = i;
+                }
+            }
+
         return location;
     }
-    private void organiseBooks()
+    //a method to organise books using a bubble sort type of algorithm, needs work still but kinda works, and by that i mean
+    //you need to manually iterate it until everything is sorted, but for now also avoid entering two titles that start
+    //with the same char.
+    public void organiseBooks()
     {
+
+
+
         int i2;
-        Vector<Book> coppy = new Vector<Book>();
-        for (int i = 0; i < index; i++)
+        //uses a vector to coppy
+        Vector<Book> libraryCoppy = new Vector<Book>();
+
+        for (int i = 0 ; i < index - 1; i++)
         {
             i2 = i + 1;
-            if(library.elementAt(i).getTitle().charAt(0) > library.elementAt(i2).getTitle().charAt(0))
+            System.out.println("iteration: " + i + " of total: " + index);
+
+            if (i != this.alphaChar(i, i2))
             {
-                coppy.addElement(library.elementAt(i2));
-                coppy.addElement(library.elementAt(i));
-            }else if(i == 0)
-            {
-                coppy.addElement(library.elementAt(i));
-                coppy.addElement(library.elementAt(i2));
-            }else
-            {
-                coppy.addElement(library.elementAt(i2));
+                for (int dex = 0; dex < i; dex++)
+                {
+                    libraryCoppy.addElement(library.elementAt(dex));
+                }
+
+                libraryCoppy.addElement(library.elementAt(i2));
+                libraryCoppy.addElement((library.elementAt(i)));
+
+                if (i2 < index - 1) {
+                    for (int dex = i2 + 1; dex < index; dex++)
+                    {
+                        libraryCoppy.addElement((library.elementAt(dex)));
+                    }
+                }
+                library.clear();
+                for (int dex = 0; dex < index; dex++)
+                {
+                    library.addElement(libraryCoppy.elementAt(dex));
+                }
+
+                //added this to help debug
+                this.viewLibrary();
             }
         }
 
-            library.equals(coppy.clone());
+    }
+    // a method to return the first item in alphabetical order, compares the first char and does not take into account
+    //when there is more than one book title with the same letter
+    private int alphaChar(int i1, int i2) {
+        char ch1 = library.elementAt(i1).getTitle().charAt(0);
+        char ch2 = library.elementAt(i2).getTitle().charAt(0);
+
+        //returns ch1 if it comes first in alphabetical order, implying a < b according to ASCII standard
+        if( ch1 < ch2)
+        {
+            System.out.println(ch1 + ", " + ch2 );
+            return i1;
+        }else{
+            System.out.println(ch2 + ", " + ch1 );
+            return i2;
+
+        }
+
 
     }
-
-
-
-
-
-
-
 
 }
 
 
-
-class UserInterface extends BookCollection
+//a user interface class
+class UserInterface //extends BookCollection
 {
+    //create library obj, a boolean to check for title, and a scanner to take user input.
     private BookCollection Library = new BookCollection();
     private boolean titleCheck;
     private Scanner in = new Scanner(System.in);
 
 
+    // main method, navigates the user input and modes with an if statement in an infinite loop
     public static void main(String args[])
     {
         // UI Object
@@ -193,11 +236,16 @@ class UserInterface extends BookCollection
             {
 
                 UI.usersLibrary();
-            }else if(mode == 3)
+            }else if(mode == 3)//remove book
             {
 
                 UI.userRemove();
-            } else if (mode == 0)//if 0 exit
+            } else if (mode==4)//secret testing mode, not displayed to user
+            {
+
+                UI.test();
+            }
+            else if (mode == 0)//if 0 exit
             {
                 System.exit(0);
 
@@ -210,20 +258,38 @@ class UserInterface extends BookCollection
 
     }
 
+    //get the mode selection from the user
     private int getModeUser()
     {
+
         System.out.println("Choose Mode: 1 = New Book  2 = View Library 3 = Remove Book 0 = Exit");
-        return in.nextInt();
+        int mode = -1;
+
+        //catch to check the int error where the input does not match
+        try {
+            mode = in.nextInt();
+        }catch (InputMismatchException e)
+        {
+            System.out.println("must enter a mode number!");
+            in.nextLine();
+        }
+
+        return mode;
     }
+    //get the title from the user
     private String getTitleFromUser()
     {
+        //
+
         titleCheck = true;
         String title = null;
+        System.out.println("Enter the Title.");
+
         while (titleCheck == true)
         {
 
 
-            System.out.println("Enter the Title.");
+
             title = in.nextLine();
 
             if (title.isEmpty() == false) //If title is not empty, exit loop
@@ -234,13 +300,13 @@ class UserInterface extends BookCollection
         }
         return title;
     }
-
+    //get the author from the user
     private String getAuthorFromUser()
     {
         System.out.println("Enter the author, or leave blank.");
         return in.nextLine();
     }
-
+    //get the title to remove from the user
     private void userRemove()
     {
         String title;
@@ -251,10 +317,12 @@ class UserInterface extends BookCollection
 
         Library.removeBook(title);
     }
+    //a method to view the library
     private void usersLibrary()
     {
         Library.viewLibrary();
     }
+    //a method to add the book, calls the get title and get author to send the information to the object
     private void userAddBook()
     {
         String title = this.getTitleFromUser();
@@ -269,5 +337,10 @@ class UserInterface extends BookCollection
             Library.addBook(title, author);
         }
     }
-
+    //secret class test, hidden from user
+    private void test()
+    {
+        System.out.println("Congratulations, you have found the secret test area.");
+        Library.organiseBooks();
+    }
 }
